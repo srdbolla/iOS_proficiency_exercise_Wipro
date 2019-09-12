a2925d27a5a6193c05863a411059c1f0950033d8
Sri Divya Bolla <bolla.divya>
2019-09-12 12:23:33 +0530

    Created project with name Wipro_iOS_Proficiency_Exercise

---
+++ Wipro_iOS_Proficiency_Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wipro_iOS_Proficiency_Exercise
+//
+//  Created by Sri Divya Bolla on 12/09/19.
+//  Copyright © 2019 Sri Divya Bolla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
